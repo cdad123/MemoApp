97acc2b020ab2851764153b9c788d8b887346047
cdad123 <wisteria1235656@yahoo.co.jp>
2023-06-10 11:07:38 +0900

    ボタンとキーボードのバグ修正

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // import { StyleSheet, View } from 'react-native';
 
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <MemoEditScreen />
   );
 }
 
