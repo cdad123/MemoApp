990fd00a4a39e033cd23cf39fc1cb4ef493eec78
cdad123 <wisteria1235656@yahoo.co.jp>
2023-06-10 16:57:30 +0900

    画面遷移

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -3,13 +3,13 @@ import {
     View, ScrollView, Text, StyleSheet,
 } from 'react-native';
 
-import AppBar from '../components/AppBar';
+
 import CircleButton from '../components/CircleButton';
 
-export default function MemoDetailScreen() {
+export default function MemoDetailScreen(props) {
+    const { navigation } = props;
     return (
         <View style={styles.container}>
-            <AppBar />
             <View style={styles.memoHeader}>
                 <Text style={styles.memoTitle}>買い物リスト</Text>
                 <Text style={styles.memoDate}>2020年12月24日 10:00</Text>
@@ -21,11 +21,16 @@ export default function MemoDetailScreen() {
                     す。本文用なので使い方を間違えると不自然に見えることもありますので要注意。
                 </Text>
             </ScrollView>
-            <CircleButton style={{top: 160, buttom: 'auto' }} name='edit-2' />
+            <CircleButton 
+                style={{top: 60, buttom: 'auto' }} 
+                name='edit-2' 
+                onPress={() => { navigation.navigate('MemoEdit'); }}
+            />
         </View>
 
     );
 }
+
 
 const styles = StyleSheet.create({
     container: {
--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import {
-    View, TextInput, StyleSheet, Keyboard, Alert,
+    View, TextInput, StyleSheet, Keyboard,
 } from 'react-native';
 
-import AppBar from '../components/AppBar';
-import CircleButton from '../components/CircleButton';
-import KeyboardSafeView from '../components/KeyboardSafeView'
 
-export default function MemoEditScreen() {
+import CircleButton from '../components/CircleButton';
+import KeyboardSafeView from '../components/KeyboardSafeView';
+
+export default function MemoEditScreen(props) {
+    const { navigation } = props;
     return (
         <KeyboardSafeView style={styles.container} behavior='height'>
-            <AppBar />
             <View style={styles.inputContainer}>
                 <TextInput value="買い物リスト" multiline style={styles.input} onSubmitEditing={Keyboard.dismiss}/>
             </View>
-            <CircleButton name="check" onPress={() => { Alert.alert('Pressed!'); }}/>
+            <CircleButton 
+                name="check" 
+                onPress={() => { navigation.goBack(); }}
+            />
         </KeyboardSafeView>
     );
 }
