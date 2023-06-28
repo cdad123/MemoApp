008d2c0634f64f728b83a59a5f59f588f56065bd
cdad123 <wisteria1235656@yahoo.co.jp>
2023-06-28 19:37:57 +0900

    update:　UXの向上”
    :wp
    wq
    :wq

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import {
-    View, Text, TextInput, StyleSheet, TouchableOpacity, Alert
+    View, Text, TextInput, StyleSheet, TouchableOpacity, Alert,
 } from 'react-native';
 import firebase from 'firebase';
 
 
 import Button from '../components/Button';
+import Loading from '../components/Loading';
+import { translateErrors } from '../utils';
 
 export default function LogInScreen(props) {
     const { navigation } = props;
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoading, setLoading] = useState(true);
 
 
     useEffect(() => {
@@ -20,12 +23,15 @@ export default function LogInScreen(props) {
                     index: 0,
                     routes: [{ name: 'MemoList' }],
                 });
+            } else {
+                setLoading(false);
             }
         });
         return unsubscribe;
     }, []);
 
     function handlePress() {
+        setLoading(true);
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then((userCredentail) => {
                 const { user } = userCredentail;
@@ -36,13 +42,17 @@ export default function LogInScreen(props) {
                 });
             })
             .catch((error) => {
-                console.log(error.code, error.message);
-                Alert.alert(error.code);
+                const errorMsg = translateErrors(error.code);
+                Alert.alert(errorMsg.title, errorMsg.description);
+            })
+            .then(() => {
+                setLoading(false);
             });
     }
 
     return (
         <View style={styles.container}>
+            <Loading isLoading={isLoading}/>
             <View style={styles.inner}>
                 <Text style={styles.title}>Log In</Text>
                 <TextInput 
